034599d1aa05e39acf9a5a77aad91ae02ac8efc5
Ariana Arianpour <ariana.suboran@gmail.com>
2024-07-17 18:39:21 +0430

    calculator is designed with HTML CSS and also basic JS.

---
+++ javascript.js
@@ -0,0 +1,10 @@
+// const equal = document.getElementById('eq');
+
+// equal.addEventListener('click',function calculate(){
+//     if(display.value === ''){
+//         alert('you must enter a digit')
+//     }
+//     else{
+//         display.value = eval(display.value)
+//     }
+// });
